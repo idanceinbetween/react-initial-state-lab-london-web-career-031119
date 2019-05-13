2c15fb9d103576cefc70c061b7b7107237c0d86a
idanceinbetween <46828701+idanceinbetween@users.noreply.github.com>
2019-05-13 19:03:52 +0100

    Done.

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,26 @@
-// your Bomb code here!
+import React, { Component } from "react";
+
+class Bomb extends Component {
+  state = {
+    secondsLeft: this.props.initialCount
+  };
+
+  constructor(props) {
+    super(props);
+  }
+
+  renderMessage() {
+    if (this.state.secondsLeft === 0) {
+      return `Boom!`;
+    }
+    return `${this.state.secondsLeft} seconds left before I go boom!`;
+  }
+
+  render() {
+    console.log("Bomb rendered");
+
+    return this.renderMessage();
+  }
+}
+
+export default Bomb;
